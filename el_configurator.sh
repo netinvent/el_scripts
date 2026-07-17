9a937cece46e1261eead3e912a18d64c09e08679
deajan <ozy@netpower.fr>
2026-07-17 21:21:53 +0200

    el_script: Fix kernel_printk sysctl syntax

--- el_configurator.sh
+++ el_configurator.sh
@@ -6,7 +6,7 @@
 # Works with Debian 13, although atm no scap profile is available as of 27-08-2025
 # Works with Ubuntu 22.04 tls, although scap support needs to be disabled as of 16-12-2025
 
-SCRIPT_BUILD="2026060301"
+SCRIPT_BUILD="2026071701"
 
 # Note that all variables can be overridden by kernel arguments
 # Example: Override BRAND_NAME with kernel argument: NPF_BRAND_NAME=MyBrand
@@ -1954,7 +1954,7 @@ if [ "${CONFIGURE_SERIAL_TERMINAL}" != false ]; then
     fi
 
     log "Optimizing for serial console speed"
-    set_conf_value /etc/sysctl.d/99-kernel_printk.conf "kernel.printk" "4 4 1 7" " "
+    set_conf_value /etc/sysctl.d/99-kernel_printk.conf "kernel.printk" "4 4 1 7"
 fi
 
 if [ "${CONFIGURE_TERMINAL_RESIZER}" != false ]; then
